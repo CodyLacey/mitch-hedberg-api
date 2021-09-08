f78ad01353d57a33719897a85741d34331ed01af
Cody Lacey <cody605@gmail.com>
2021-09-08 18:07:23 -0500

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
+const cors = require('cors')
 require('dotenv').config()
 
 //mongodb connect
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     db.collection('quotes').find().toArray()
