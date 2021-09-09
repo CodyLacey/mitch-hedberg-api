9173c4615d586e42fa2ea9a69fc2337aa5744e68
Cody Lacey <cody605@gmail.com>
2021-09-08 22:58:09 -0500

    update

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,24 @@ function getQuote() {
 }
 getQuote()
 document.getElementById('refresh').addEventListener('click', () => getQuote())
+
+let codeBlock = {
+
+    'javascript' : `fetch('https://mitch-hedberg-api.herokuapp.com')<br/>
+                    .then(res => res.json())<br/>
+                    .then(data => console.log(data.quote.quote);`
+}
+
+document.getElementById('code').innerHTML = codeBlock.javascript
+
+faqStatus = false;
+document.getElementById('faq').addEventListener('click', () => {
+    let faqSection = document.getElementById('faqSection');
+    if (!faqStatus) {
+        faqSection.style.height = '300px';
+        faqStatus = true;
+    } else {
+        faqSection.style.height = '0px';
+        faqStatus = false;
+    }
+});
